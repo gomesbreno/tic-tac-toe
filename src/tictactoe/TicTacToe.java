6806466036c056c78669baeb0e4a21e96121c74e
Breno Gomes <brenodmelogomes@outlook.com>
2018-03-27 12:02:37 -0300

    Add files via upload

---
+++ src/tictactoe/TicTacToe.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+import Interface.TicTacToeInterface;
+
+/**
+ *
+ * @author Breno
+ */
+public class TicTacToe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TicTacToeInterface interfaceJogo = new TicTacToeInterface();
+        interfaceJogo.setVisible(true);
+    }
+    
+}
